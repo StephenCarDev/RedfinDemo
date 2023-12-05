e90c171c69b391eeb5808e6368833b3811a508d3
zhanfeng <zhanfeng990818@126.com>
2023-12-05 11:32:11 +0800

    refactor:更新签名文件与gradle配置

--- app/src/main/java/com/stephen/redfindemo/view/MainActivity.kt
+++ app/src/main/java/com/stephen/redfindemo/view/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.stephen.redfindemo.view
 
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.stephen.redfindemo.R
 import com.stephen.redfindemo.base.appContext
+import com.stephen.redfindemo.utils.debugLog
 import com.stephen.redfindemo.utils.infoLog
 
 class MainActivity : AppCompatActivity() {
@@ -13,15 +18,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         getAppList()
+
+        findViewById<TextView>(R.id.id_battery).text = getBatteryValue()
     }
 
     private fun getAppList() {
         val packageManager = appContext.packageManager
         val appInfoList = packageManager.getInstalledApplications(0)
-        appInfoList.sortedBy { it.loadLabel(packageManager).toString() }
+        appInfoList.filter { it.name != null }
+            .sortedBy { it.loadLabel(packageManager).toString() }
             .forEach {
-                infoLog("appItem: packageName: ${it.packageName}, name: ${it.name}")
+                debugLog("appItem: packageName: ${it.packageName}, name: ${it.name}")
             }
+    }
+
+    private fun getBatteryValue(): String {
+        val ifilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+        val batterystatus = appContext.registerReceiver(null, ifilter)!!
+        val level = batterystatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+        val scale = batterystatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+        val batteryPct = scale / level * 100
+        infoLog("battery: scale:$scale, level:$level , batteryPct:$batteryPct%")
+        return batteryPct.toString()
     }
 
     override fun onStop() {
